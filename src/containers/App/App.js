32c7f8485bba28960499a63314216ccc81c0dadf
posi7790 <posi7790@colorado.edu>
2019-10-30 11:36:50 -0600

    Fetch exoplanet data

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.scss';
 import { Route } from 'react-router-dom';
+import NavigationBar from '../NavigationBar/NavigationBar';
 import AccountManager from '../../components/AccountManager/AccountManager';
 import Footer from '../../components/Footer/Footer';
 import { bindActionCreators } from 'redux';
@@ -13,9 +14,14 @@ class App extends Component {
 
   async componentDidMount() {
     const { checkIsLoading, hasErrored } = this.props;
-    //https://api.betterdoctor.com/2016-03-01/doctors?name=%3Aname&first_name=%3Afirst_name&last_name=%3Alast_name&query=%3Aquery&specialty_uid=%3Aspecialty_uid&insurance_uid=%3Aspecialty_uid&practice_uid=%3Apractice_uid&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&gender=male&
-    var apiBaseUrl = "https://api.betterdoctor.com/2016-03-01/";
-    const url = `${apiBaseUrl}doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=1ac2abd6f94242154ff7cef2b1137a83`;
+    //https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?&table=exoplanets&format=ipac&where=pl_kepflag=1
+    const apiBaseUrl = "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?";
+    // const api_key = "";
+    const table = "table=exoplanets";
+    const columns = "&select=pl_hostname,ra,dec";
+    const parameters = "&order=dec";
+    const format = "&format=json"
+    const url = `${apiBaseUrl}${table}${columns}${parameters}${format}`;
     try {
       checkIsLoading(true);
       const data = await getData(url);
@@ -31,6 +37,7 @@ class App extends Component {
   render() {
     return (
       <main className='App'>
+        <NavigationBar />
         <Route path='/account' render={() => (<AccountManager />)} />
         <Footer />
       </main>
---
+++ src/containers/NavigationBar/NavigationBar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Fade from '@material-ui/core/Fade';
+import PropTypes from 'prop-types';
+
+const NavigationBar = () => {
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <nav>
+      <Button aria-controls="fade-menu" aria-haspopup="true" onClick={handleClick}>
+        Menu
+      </Button>
+      <Menu
+        id="fade-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Fade}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </nav>
+  )
+}
+
+export default NavigationBar;
