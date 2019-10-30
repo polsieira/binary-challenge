a4b27d6a1b35870906e7634cf4469e4292820a4d
posi7790 <posi7790@colorado.edu>
2019-10-29 20:36:08 -0600

    Add thunk

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { HashRouter } from 'react-router-dom'
 import rootReducer from './reducers';
+import thunk from 'redux-thunk';
+
 import App from './containers/App/App';
 import './index.scss';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
