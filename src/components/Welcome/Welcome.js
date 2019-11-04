f5a9ce857616f5be12bdbb2c31709e189501a9dd
posi7790 <posi7790@colorado.edu>
2019-11-04 10:05:15 -0700

    Add welcome banner and style

---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Welcome.scss';
+
+const Welcome = () => {
+
+  return (
+    <div className="Welcome">
+      <h1 className="welcome-message">Don't limit yourself to just one planet</h1>
+    </div>
+  )
+
+
+}
+
+export default Welcome;
