141bf105948d84a72c54ca1475e6a969ebcd619e
posi7790 <posi7790@colorado.edu>
2019-11-04 09:54:44 -0700

    Clean up AcccountManager css and add basic snapshot test

--- src/components/AccountManager/AccountManager.js
+++ src/components/AccountManager/AccountManager.js
@@ -1,32 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Login from '../../containers/Login/Login';
 import CreateAccount from '../../containers/CreateAccount/CreateAccount';
 import './AccountManager.scss';
-import { Route, NavLink, Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import { Route, NavLink } from 'react-router-dom';
 
-export const AccountManager = () => {
-  return (
-    <>
-      <div className='modal-container'>
-        <ul className='modal-tabs'>
-          <NavLink to='/account/login' className='form-navlink'>
-            <li className='login_tab'>Log In</li>
-          </NavLink>
-          <NavLink to='/account/create-account' className='form-navlink'>
-            <li className='login_tab'>Create Account</li>
-          </NavLink>
-        </ul>
-        <Route exact path='/account/login' render={() => <Login />}
-        />
-        <Route exact path='/account/create-account' render={() => <CreateAccount />} />
+class AccountManager extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selected: 'login'
+    }
+  }
+
+  changeSelected = (id) => {
+    this.setState({ selected: id });
+  }
+
+  render() {
+    return (
+      <div className='AccountManager' >
+        <div className='modal-container'>
+          <ul className='modal-tabs'>
+            <NavLink to='login' className={this.state.selected === 'login' ? 'form-navlink form-navlink--left' : 'form-navlink form-navlink--left unselected'} onClick={() => this.changeSelected('login')}>
+              <li className='login_tab'> Log In</li>
+            </NavLink>
+            <NavLink to='/create-account' className={this.state.selected === 'create account' ? 'form-navlink form-navlink--right' : 'form-navlink form-navlink--right unselected'} onClick={() => this.changeSelected('create account')}>
+              <li className='login_tab'>Create Account</li>
+            </NavLink>
+          </ul>
+          <Route exact path='/login' render={() => <Login />}
+          />
+          <Route exact path='/create-account' render={() => <CreateAccount />} />
+        </div>
       </div>
-    </>
-  );
+    );
+  }
 };
 
 export default AccountManager;
-
-AccountManager.propTypes = {
-
-};
---
+++ src/components/AccountManager/AccountManager.test.js
@@ -0,0 +1,22 @@
+import AccountManager from './AccountManager'
+import { shallow } from 'enzyme';
+import React from 'react';
+
+
+describe('AccountManager', () => {
+
+  let wrapper;
+
+  describe('AccountManager component', () => {
+    beforeEach(() => {
+      wrapper = shallow(<AccountManager />)
+    })
+
+    it('should match a snapshot', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+
+  });
+
+});
+
