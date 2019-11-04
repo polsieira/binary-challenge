63535f070222e7882d2fbedc59983b6703f9e1ab
posi7790 <posi7790@colorado.edu>
2019-11-04 09:59:03 -0700

    Import and combine neccesary reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import { isLoading } from './isLoading';
 import { hasErrored } from './hasErrored';
 import { exoplanets } from './exoplanets';
 import { user } from './user';
+import { filters } from './filters';
 
 const rootReducer = combineReducers({
   isLoading,
   error: hasErrored,
   exoplanets,
-  user
+  user,
+  filters
 });
 
 export default rootReducer;
